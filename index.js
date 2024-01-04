12d36a8538bde6b978ca05342787cd14988caf0b
Eduardo Morais <dudu.vgvm@gmail.com>
2024-01-04 17:59:46 -0300

    Commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-export { somaHorasExtras, calculaDescontos };
+export {
+  somaHorasExtras,
+  calculaDescontos,
+};
